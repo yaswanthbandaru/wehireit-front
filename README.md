c380f8a34daf6e628a391ed3be37c4d9da06b41c
yaswanthbandaru <yaswanthbandaru424@gmail.com>
2022-11-25 14:27:46 +0530

    initial commit and built a router frontend

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+[Link](https://www.geeksforgeeks.org/create-a-responsive-navbar-using-reactjs/)
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Events from './pages/events';
+import AnnualReport from './pages/annual';
+import Teams from './pages/team';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+	<Router>
+		<Navbar />
+		<Routes>
+			{/* <Route path='/' exact component={Home} /> */}
+			{/* <Route exact path='/'> <Home /> </Route> */}
+			{/* <Route path='/about' component={About} /> */}
+			{/* <Route path='/about'> <About /> </Route> */}
+			{/* <Route path='/events' component={Events} /> */}
+			{/* <Route path='/annual' component={AnnualReport} /> */}
+			{/* <Route path='/team' component={Teams} /> */}
+			{/* <Route path='/blogs' component={Blogs} /> */}
+			{/* <Route path='/sign-up' component={SignUp} /> */}
+			<Route path='/about' element={<About />} />
+			<Route path='/events' element={<Events />} />
+			<Route path='/annaul' element={<AnnualReport />} />
+			<Route path='/team' element={<Teams />} />
+			<Route path='/blogs' element={<Blogs />} />
+			<Route path='/sign-up' element={<SignUp />} />
+			<Route path='/' element={<Home />} />
+		</Routes>
+	</Router>
+);
 }
 
 export default App;
+
